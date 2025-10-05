b5f98d301e7d35573416ad078c4b5f5a9e8f037f
Bianca Livonius <137654515+buzz4biz@users.noreply.github.com>
2025-10-05 17:22:55 -0300

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
